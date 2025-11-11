957d03905b957424a7e0a048356cc98b1d02de16
SajadWritesCodes <sajjadshahriari98@hotmail.com>
2025-11-10 17:51:12 -0800

    Update inputs by showing multipe options to user

--- expense_tracker.py
+++ expense_tracker.py
@@ -7,16 +7,19 @@ def main():
 
 def get_inputs():
     
+    commands = """
+                1. ADD 
+                2. FILTER BY CATEGORY
+                3. TOTAL SPENDING
+                4. TOTAL SPENDING BY CATEGORY
+                """
     while True:
-        commend = input("'choose one of these list, you can do it by just typing the number\n' 
-                         , '1. ADD   \n'
-                         , '2. Filter     \n'"
-                         )
-        if commend == '1':
+        command = input(commands)
+        if command == '1':
             amount = int(input('How much you spend? '))
             category = input('What was it for? ')
             adding_to_list(amount, category)
-        elif commend == '2':
+        elif command == '2':
             category = input('Which category you want to filter? ')
             filter_by_category(category)
         else :
@@ -29,9 +32,7 @@ def adding_to_list(amount,category):
 
     return expenses
 """
-so I was trying too get commend by offering a menu 
-problem : how to print a menu in input function,
-then after I solve that 
+ 
 I should write the filter function, it is suppose to show total expenses of filtered category
 
 """
