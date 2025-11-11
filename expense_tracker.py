6c4d213c06fabbde901c6b7a09f41b2f6c25ce31
SajadWritesCodes <sajjadshahriari98@hotmail.com>
2025-11-10 19:53:32 -0800

    Finished it

--- expense_tracker.py
+++ expense_tracker.py
@@ -9,9 +9,11 @@ def get_inputs():
     
     commands = """
                 1. ADD 
-                2. FILTER BY CATEGORY
-                3. TOTAL SPENDING
-                4. TOTAL SPENDING BY CATEGORY
+                2. SHOW ALL EXPENSES
+                3. FILTER EXSPENSES BY CATEGORY
+                4. TOTAL EXPENSES
+                5. TOTAL EXPENSES BY CATEGORY
+                6. EXIT
                 """
     while True:
         command = input(commands)
@@ -19,36 +21,46 @@ def get_inputs():
             amount = int(input('How much you spend? '))
             category = input('What was it for? ')
             adding_to_list(amount, category)
+
         elif command == '2':
+            print(expenses)
+
+        elif command == '3':
             category = input('Which category you want to filter? ')
-            filter_by_category(category)
-        else :
+            print(filter_by_category(category, expenses))
+
+        elif command == '4':
+            print(total_sum(expenses))
+        
+        elif command =='5':
+            category = input('Which category you want to filter? ')
+            print(sum(map(lambda x: x['amount:'],filter_by_category(category, expenses))))
+        
+        elif command == '6':
+            print("See ya later!")
+            
             break
-    return expenses
+
+        else :
+            print('Wrong command, please type the number of your choice.')
 
 
-def adding_to_list(amount,category):
+def adding_to_list(amount, category):
     expenses.append({'amount:': amount , 'category': category})
 
     return expenses
-"""
- 
-I should write the filter function, it is suppose to show total expenses of filtered category
 
-"""
+def filter_by_category(category, expenses):
+    idk = lambda expense: expense['category'] == category
+    
+    filtered_list =list( filter(idk , expenses))
 
-
-
-
-
-
-
-
-
-
-
-
-
+    return filtered_list
+   
+    
+def total_sum(expenses):
+    total = (sum(map(lambda expense: expense['amount:'],  expenses)))
+    return total
 
 
 
