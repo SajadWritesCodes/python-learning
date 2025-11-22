53bde043cd26dd30bdfa95d090476b1e11795a63
SajadWritesCodes <sajjadshahriari98@hotmail.com>
2025-11-22 10:22:34 -0800

    spliting the arguments

---
+++ arithmetic_formatter.py
@@ -0,0 +1,18 @@
+"""
+def arithmetic_arranger(problems, show_answers=False):
+
+    return problems
+
+print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+"""
+
+def arithmetic_arranger(problems):
+        
+        for problem in problems:
+            splited_args = problem.split()
+            for splited_arg in splited_args:
+                
+
+
+
+print(arithmetic_arranger(["3801 - 2", "123 + 49"]))
