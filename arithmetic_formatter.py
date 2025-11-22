52b26064adefedcb956bc252855dee2c10a15c36
SajadWritesCodes <sajjadshahriari98@hotmail.com>
2025-11-22 10:52:36 -0800

    buildig list of dicts with keys of first arg, operator, second arg

--- arithmetic_formatter.py
+++ arithmetic_formatter.py
@@ -7,10 +7,16 @@ print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}
 """
 
 def arithmetic_arranger(problems):
-        
+        list_of_dicts = []
+        new_dict = {}
         for problem in problems:
             splited_args = problem.split()
-            for splited_arg in splited_args:
+            new_dict["first arg"] = splited_args[0]
+            new_dict["operator"] = splited_args[1]
+            new_dict["second arg"]= splited_args[2]
+            list_of_dicts.append(new_dict)
+        return list_of_dicts
+                
                 
 
 
