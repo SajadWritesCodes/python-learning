86aacc9b9b59bb9bdfcaa451885ab016a4f90f4e
SajadWritesCodes <sajjadshahriari98@hotmail.com>
2025-11-22 11:24:37 -0800

    completing data set

--- arithmetic_formatter.py
+++ arithmetic_formatter.py
@@ -7,18 +7,37 @@ print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}
 """
 
 def arithmetic_arranger(problems):
-        list_of_dicts = []
-        new_dict = {}
-        for problem in problems:
-            splited_args = problem.split()
-            new_dict["first arg"] = splited_args[0]
-            new_dict["operator"] = splited_args[1]
-            new_dict["second arg"]= splited_args[2]
-            list_of_dicts.append(new_dict)
-        return list_of_dicts
+    arithmetic_sorter(raw_data(problems))
+    pass
+def raw_data(problems):
+    list_of_dicts = []
+    new_dict = {}
+    for problem in problems:
+        splited_args = problem.split()
+        new_dict["first arg"] = splited_args[0]
+        new_dict["operator"] = splited_args[1]
+        new_dict["second arg"]= splited_args[2]
+        if new_dict["operator"] == "+":
+            new_dict["answer"] = str(int(splited_args[0]) + int(splited_args[2]))
+        else :
+            new_dict["answer"] = str(int(splited_args[0]) - int(splited_args[2]))
+            
+        if len(new_dict["first arg"]) > len(new_dict["second arg"]):
+            counter = len(new_dict["first arg"])
+        else:
+            counter = len(new_dict["second arg"])
+            
+        new_dict["lines"] = (counter+2) * "-"
+        list_of_dicts.append(new_dict)
+        new_dict ={}
+    
+    return list_of_dicts
+
+def arithmetic_sorter():
+    
                 
                 
 
 
 
-print(arithmetic_arranger(["3801 - 2", "123 + 49"]))
+print(raw_data(["3801 - 2", "123 + 49"]))
