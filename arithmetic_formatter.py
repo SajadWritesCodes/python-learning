d42df19c025a35a0121a5c39898e77cbb7b9221a
SajadWritesCodes <sajjadshahriari98@hotmail.com>
2025-11-22 17:48:26 -0800

    Adding some error handling and exceptions

--- arithmetic_formatter.py
+++ arithmetic_formatter.py
@@ -1,7 +1,7 @@
 
-def arithmetic_arranger(problems):
-    arithmetic_sorter(raw_data(problems))
-    pass
+def arithmetic_arranger(problems, show_answers=False):     
+    arithmetic_sorter(raw_data(problems), True)
+    
 def raw_data(problems):
     list_of_dicts = []
     new_dict = {}
@@ -19,10 +19,16 @@ def raw_data(problems):
         
             
         new_dict["lines"] = (counter+2) * "-"
-        if new_dict["operator"] == "+":
-            new_dict["answer"] = str(int(splited_args[0]) + int(splited_args[2]))
-        else :
-            new_dict["answer"] = str(int(splited_args[0]) - int(splited_args[2]))
+        try:
+            if new_dict["operator"] == "+":
+                new_dict["answer"] = str(int(splited_args[0]) + int(splited_args[2]))
+            elif new_dict["operator"] == "-" :
+                new_dict["answer"] = str(int(splited_args[0]) - int(splited_args[2]))
+            else: 
+                return "Error: Operator must be '+' or '-'."
+        except ValueError:
+            return "Error: Numbers must only contain digits."
+
        
         new_dict["first arg"] = ((len(new_dict["lines"]) - len(new_dict["first arg"])) * " ") + splited_args[0]
         new_dict["second arg"] = (((len(new_dict["lines"]) - len(new_dict['second arg']) -1)) * " ") + splited_args[2]
@@ -30,31 +36,38 @@ def raw_data(problems):
 
 
         list_of_dicts.append(new_dict)
+        if len(list_of_dicts)> 5 :
+            return 'Error: Too many problems.'
         new_dict ={}
     
     return list_of_dicts
 
-def arithmetic_sorter(data_set):
-    print(data_set)
-    for _ in data_set:
-        first_arg = _["first arg"]
-        print (first_arg + "    ", end="")
-    print("")
-    for sec_line in data_set:
-        operator = sec_line["operator"]
-        second_arg = sec_line['second arg']
-        print(operator+second_arg+"    ", end="")
-    print()
-    for lines in data_set:
-        line = lines['lines']
-        print(line+"    ", end="")
-    print("")
-    for answers in data_set:
-        answer = answers["answer"]
-        print(answer + "    " , end="")
-    print('')
+def arithmetic_sorter(data_set, show_answers=False):
+    if data_set is list:
+        for _ in data_set:
+            first_arg = _["first arg"]
+            print (first_arg + "    ", end="")
+        print("")
+        for sec_line in data_set:
+            operator = sec_line["operator"]
+            second_arg = sec_line['second arg']
+            print(operator+second_arg+"    ", end="")
+        print()
+        for lines in data_set:
+            line = lines['lines']
+            print(line+"    ", end="")
+        print("")
+        if show_answers == True:
+            for answers in data_set:
+                answer = answers["answer"]
+                print(answer + "    " , end="")
+            print('')
+        else: 
+            pass
+    else :
+        print(data_set)
 
 
-arithmetic_arranger(["3801 - 3802", "123 + 49", "11 - 111"])  
+arithmetic_arranger(["3801 - 3802", "1h3 + 49", "11 - 111", "122 - 1222","111 - 18", "26 - 2666"])  
 
 
